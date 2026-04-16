6d60df74a419e7e6842a3d157d729788d80d1294
Tom Magnusson <tommagnuss@gmail.com>
2026-04-15 23:25:35 -0400

    discards ingredients from potions after use

--- Nodes/DiscardPile.gd
+++ Nodes/DiscardPile.gd
@@ -21,7 +21,14 @@ func transfer_out() -> Array[Ingredient]:
 	return out
 	
 func updateDiscardPileCountLabel():
+	# TODO: have this scene shake or highlight or something to let player know it's been updated
 	discardPileCountLabel.text = str(pile.size())
+	
+func release_ingredients_in_potion(potion: Potion):
+	for ingredient in potion.ingredients:
+		pile.append(ingredient)
+		
+	updateDiscardPileCountLabel()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
